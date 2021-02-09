bd972066c96e8689a4b93b8cd184a033cae973c0
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2021-02-09 11:00:16 -0800

    run cadence strategies on docker restart

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+python manage.py runcadencestrategies
 crontab crontab.txt
 gunicorn -b 0.0.0.0:8080 snex2.wsgi
